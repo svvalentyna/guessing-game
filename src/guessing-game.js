c3dedf4e4f680f4ae3cd3b8b93a78ce0fb7d8116
svvalentyna <svvalentyna@gmail.com>
2022-08-16 22:58:42 +0300

    feat: task guessing-game

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,19 +1,21 @@
 class GuessingGame {
     constructor() {}
-
+    
     setRange(min, max) {
-
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        return Math.round((this.max - this.min)/2 + this.min);
     }
 
     lower() {
-
+        return this.max = Math.round((this.max - this.min)/2 + this.min);
     }
-
+    
     greater() {
+        return this.min = Math.round((this.max - this.min)/2 + this.min);
 
     }
 }
